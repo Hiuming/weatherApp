4183ab335e129e6e633fdee51958d1ae5c6d71c1
Minh Hieu <hieuminh282000@gmail.com>
2021-06-08 13:50:42 +0700

    Initial Commit

---
+++ weather_app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather_app
+//
+//  Created by Huynh Minh Hieu on 08/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
